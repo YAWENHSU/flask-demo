e434db915f86f20b6a1739c5e2bd2d89a0c79438
Tina <ddyy2864@gmail.com>
2023-11-15 14:55:15 +0800

    輸出書本資訊

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 from datetime import datetime
 
 app = Flask(__name__)
+books = {1: "Python book", 2: "Java book", 3: "Flask book"}
 
 # 首頁
 
@@ -11,13 +12,42 @@ app = Flask(__name__)
 def index():
     today = datetime.now()
     print(today)
-    return f'Hello world!{today}'
+    return render_template('index.html', today=today)
 
 
-@app.route('/books')
+@app.route('/bmi/name=<name>&weight=<w>&height=<h>')
+def BMI_Calc(name, w, h):
+    bmi = round(eval(w)/(eval(h)/100)**2, 2)
+    return {"bmi": bmi}
+
+
+@app.route("/books")
 def get_all_books():
-    books = {1: "Python book", 2: "Java book", 3: "Flask book"}
-    return books
+    books = {
+        1: {
+            "name": "Python book",
+            "price": 299,
+            "image_url": "https://im2.book.com.tw/image/getImage?i=https://www.books.com.tw/img/CN1/136/11/CN11361197.jpg&v=58096f9ck&w=348&h=348"
+        },
+
+        2: {
+
+            "name": "Java book",
+            "price": 399,
+            "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/087/31/0010873110.jpg&v=5f7c475bk&w=348&h=348"
+        },
+
+        3: {
+            "name": "C# book",
+            "price": 499,
+            "image_url": "https://im1.book.com.tw/image/getImage?i=https://www.books.com.tw/img/001/036/04/0010360466.jpg&v=62d695bak&w=348&h=348"
+        },
+    }
+
+    for id in books:
+        print(id, books[id]["name"], books[id]
+              ["price"], books[id]["image_url"])
+    return render_template("books.html", books=books)
 
 
 @app.route('/books/<int:id>')
@@ -28,12 +58,6 @@ def get_books(id):
     except Exception as e:
         print(e)
     return '<h1>書籍編號不正確</h1>'
-
-
-@app.route('/bmi/name=<name>&weight=<w>&height=<h>')
-def BMI_Calc(name, w, h):
-    bmi = round(eval(w)/(eval(h)/100)**2, 2)
-    return {"bmi": bmi}
 
 
 if __name__ == '__main__':
