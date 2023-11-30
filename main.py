c4b575a6a2948b6b608adee805bc0f8334ef8f56
Tina <ddyy2864@gmail.com>
2023-11-30 15:51:17 +0800

    1.完成pm2.5表格頁面 2.新增排序跟最大最小顯示功能

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from datetime import datetime
 import pandas as pd
 
 app = Flask(__name__)
 books = {1: "Python book", 2: "Java book", 3: "Flask book"}
+ascending = True
 
 # 首頁
 
@@ -61,14 +62,31 @@ def get_books(id):
     return '<h1>書籍編號不正確</h1>'
 
 
-@app.route("/pm25")
+@app.route("/pm25", methods=["GET", "POST"])
 def get_pm25():
+    global ascending
     url = 'https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=CSV'
     now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    sort = False
+    # 確定回傳方法
+    if request.method == "POST":
+        if request.form.get('sort'):
+            sort = True
+
+    # 製作升降序功能
     try:
         df = pd.read_csv(url).dropna()
+        if sort:
+            df = df.sort_values("pm25", ascending=ascending)
+            ascending = not ascending
+        else:
+            ascending = True
         columns = df.columns.tolist()
         values = df.values.tolist()
+
+        lowest = df.sort_values('pm25').iloc[0][['site', 'pm25']].values
+        highest = df.sort_values('pm25').iloc[-1][['site', 'pm25']].values
+
         message = "取得資料成功!"
     except Exception as e:
         print(e)
